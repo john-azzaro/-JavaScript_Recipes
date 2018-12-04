5d4cd1c3ad5f4c83ff75fc4734f4b1440f78f9cc
John Azzaro <jazzaro@temple.edu>
2018-12-03 22:00:44 -0800

    PTOF additions

--- palindromeTOF.js
+++ palindromeTOF.js
@@ -1,1 +1,8 @@
 // palindrome True or false
+//-- Question: Given a string, return true if the string is a palindrome or false if it is not.
+//-- Palindromes are strings that form the same word if it is reversed.
+//-- Example: abba === true
+//            abcd === false
+
+
+///// Solutions ///////////////////////////////////////////////////////
