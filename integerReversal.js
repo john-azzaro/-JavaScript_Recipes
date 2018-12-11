4476e031ff6ea3ab8a7b602c6eee5ecbec8c7a71
John Azzaro <jazzaro@temple.edu>
2018-12-10 20:50:09 -0800

    additions

--- integerReversal.js
+++ integerReversal.js
@@ -17,7 +17,10 @@ The solution is very stright forward but its all about understanding the trick t
 // so I pass in an interger like 15 and the numbers should reverse... 51.
 // You can do the strign reversal methods, but since this is an integer we want to do it this way.
 
-// METHOD 1:
+
+
+// METHOD 1: toString() /////////////////////////////////////////////////////
+
 // We know how to reverse a string... (i.e. return string.split('').reverse().join('')).
 // But numbers are different in this case.
 // We CAN turn a number into a string by using a function called toString().
@@ -32,3 +35,30 @@ myNumber.toString().split('').join('');
 
 // SO IN SUM: we can use the toString() to turn our number into a string and then work on it as though it was a string.
 
+
+
+// Trick 1: Math.sign() ////////////////////////////////////////////////////////
+
+// This method uses a function in the math library included with JavaScript called Math.sign().
+// With Math.sign() we can pass in a number. If that number is positive, Math.sign() will return 1.  If negative, will return -1.
+// So as an example:
+
+Math.sign(4000) // returns 1
+Math.sign(-4000)  // returns -1
+
+// so this helper function is useful because it might help solve the issue of maintaining the sign of the actual number.
+
+
+
+// Trick 2: parseInt ////////////////////////////////////////////////////////////
+
+// When using the toString() to turn a number into a string using a string.  
+// For example...
+
+const myNumer2 = 400;
+// again, using toString by splitting it and joining it back together, we are still working with a string.
+// To turn all of this back into a number, you can use the parseInt() function.
+
+parseInt(myNumber2.toString())
+
+// parseInt() takes a string (specifically, myNumber2.toString()) and it returns a number.
