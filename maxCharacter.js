cc368080f8940e1d705f17616ec46d9b70e32927
John Azzaro <jazzaro@temple.edu>
2018-12-12 20:59:25 -0800

    maxchar additions

---
+++ maxCharacter.js
@@ -0,0 +1,8 @@
+/// max characters /////////////
+/* 
+given a string, return the character that is most commonly used in the string.
+example: maxChar("abcccccccd") === "c"
+
+NOTE: This technique that we're goign to use to solve can be used in many other string related questions.
+
+*/
