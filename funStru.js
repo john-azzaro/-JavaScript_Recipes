c6d9009d3a6d26c4603f0951a1116cd5709b5fb1
John Azzaro <jazzaro@temple.edu>
2018-12-14 21:21:26 -0800

    initial commit

---
+++ funStru.js
@@ -0,0 +1,27 @@
+
+
+function flip(string) {
+    return string.split('').reverse().join('');
+}
+
+function print(string) {
+    console.log(string);
+}
+
+function replace(string) {
+     return string.replace("this","that");
+ }
+
+function replaceFlipPrint(string) {
+    print(flip(replace(string)));
+}
+
+
+function variablesReplaceFlipPrint(string) {
+    let replaced = replace(string);
+    let flipped = flip(replaced);
+    print(flipped); 
+}
+
+variablesReplaceFlipPrint("this is a tester first");
+replaceFlipPrint("this is a tester second");
