2ad1014c524ec28583525583b5e8380d80deef6c
John Azzaro <jazzaro@temple.edu>
2018-12-24 09:55:01 -0800

    ASO combo array

--- arraySpreadOperator.js
+++ arraySpreadOperator.js
@@ -1,29 +1,50 @@
 "use strict";
-// WHAT IS THIS DOCUMENT? /////////////////////////////////////////////////////////////////////////////////////////////////
-// Summary: 
-// Purpose: 
-// Objective:
-
-// WHAT DOES THIS DOCUMENT DO? ////////////////////////////////////////////////////////////////////////////////////////////
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//    Using the array spread operator
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
 // What is the problem?:  
-// Why hasn't the problem been solved?: 
-// What is possible?: 
-// What is different?: 
+//    
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the BEST solution? 
+//     
+
+// What are the special components of these solutions?:  
+//     1. combining arrays
+
+// What needs work?
+//     1. copying arrays
+//     2. convert arguments to array
+//     3. using math functions
+//     4. destructuring
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+// COMBINING ARRAYS /////////////////////////////////////////////////
 
-numbers = [1,2,3,4,5];
-multiplier = [6,7,8,9];
+const numbersCA = [1,2,3,4,5];
+const multiplierCA = [6,7,8,9];
 
+// WITHOUT ARRAY SPREAD OPERATOR //
 // includes the mulitplier as a single array added to numbers
-console.log(numbers.push(multiplier));
+// So bascially when you push multiplierCA into numbersCA, the multiplierCA is simply pushed into the array as a single entity.
+console.log(numbersCA.push(multiplierCA));
 
-
+// WITH ARRAY SPREAD OPERATOR //
 // includes the multiplier numbers with the numbers
-console.log(numbers.push(...multiplier));
+// However, when you use the array spread operator, each item in the multiplierCA array is pushed into the numbersCA array.
+console.log(numbersCA.push(...multiplierCA));
 
 
+
+// USING MATH FUNCTIONS ////////////////////////////////////////////
+
+let numbersMF = [9, 4, 7, 1];
+
+// WITH
+
+
+console.log(Math.min(...numbersMF));
+console.log(Math.max(...numbersMF));
 
