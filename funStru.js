389f85e8ff4b49640ed6d62d67f20d1234f718d5
John Azzaro <jazzaro@temple.edu>
2018-12-26 21:55:19 -0800

    ammended function structure, cleaned code, added commentary

--- funStru.js
+++ funStru.js
@@ -1,5 +1,4 @@
 "use strict";
-
 // OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Outline of interesting scripting structures and problem solving methodologies from research, discussions, etc.
 // SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -13,27 +12,24 @@
 //     1. First and foremost, the general structure of a function itself, including ...
 
 // What needs work?
-//     1. more interesting ways to think about scripting strucuture 
+//     1. FAQ section (neat questions and answers I come across I'll just add to the list)
+//     2. More interesting ways to think about scripting strucuture 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 
+// WHAT ARE THE BASIC COMPONENTS OF A FUNCTION?:
+//              A function is a repeatable (i.e. can be called multiple times) and determinate (i.e. predictable) process or behavior.
+//              In the example function below we are creating a function that simply adds two numbers together. 
+//              Inside the call signature, we have two parameters, num1 and num2.  These are the two numbers that we add together.
+//              Inside the block of the function, we have a simple goal... to return the value of num1 plus num2. 
 
-///// ANATOMY OF A FUNCTION //////////////////////////////////////
-
-// WHAT IS A FUNCTION?:
-// A function is a repeatable (i.e. can be called multiple times) and determinate (i.e. predictable) process or behavior.
-
-// WHAT COMPRISES A FUNCTION?:
-
-
-                function add(num1, num2) {                  
-                    return num1 + num2;                           
-                }
-                console.log(add(2,3));     // 5
-
+                    function addExample(num1, num2) {
+                        return num1 + num2;
+                    }
+                    console.log(addExample(1,1));
 
 //              1. "function" referes to the JavaScript keyword about to be described.
 //              2. "add" is the name of the function we can invoke elsewhere in the code.
@@ -41,88 +37,37 @@
 //              4. "{ ... }" brackets are the main block of the function to be executed.
 //              5. "console.log(add(2,3))" is the line that invokes the function, the numbers in the callsignature inserted into num1 and num2.
 
-// Function 
+
+
+
+///// HOW CAN YOU DEFINE A FUNCTION?:
+
+    // Function Declaration:
+
+                    function addDeclaration(num1, num2) {
+                        return num1 + num2;
+                    }
+                    console.log(addDeclaration(2,2))     // 4
+
+    // Function Expression:
+
+                    const addExpression = function(num1, num2) {
+                        return num1 + num2;
+                    }
+                    console.log(addExpression(3,3));    // 6
+
+    // ES6 Fat Arrow Function:
+
+                    const addFatArrow = (num1, num2) => num1 + num2;
+                    console.log(addFatArrow(4,4))       // 8   
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// useful problem solving strategies
-
-// 1. Always have a plan
-// 2. Restate the problem
-// 3. Divide the Problem
-// 5. Start with what you know
-// 6. Reduce the Problem
-// 7. Look for Analogies
-// 8. Experiment
-// 9. Dont get frustrated
-// 10.
-
-
-
-
-
-// multiply and divide and number, using another function in operation
-
-function minus(divide) {
-    return divide - 1;
-}
-
-function multiplyDivide2(number, numberToMultiply, numberToDivide) {
-    const start = number;
-    const multiply = start * numberToMultiply;
-    const divide = multiply / numberToDivide;
-    const minused = minus(divide)
-    return minused;
-}
-
-console.log(multiplyDivide2(2,5,2));
-
-
-
-
-
-// multiplyt and divide a number via chaining variables.
-
-
-function multiplyDivide(number, numberToMultiply, numberToDivide) {
-    const start = number;
-    const multiply = start * numberToMultiply;
-    const divide = multiply / numberToDivide;
-    return divide;
-}
-
-console.log(multiplyDivide(2,5,2));
-
-
-
-
-
-// Example that demonstrates the interconnectivity of a function
-// first pay attention tot he arguments in the call signtature.  These are "slots".  In this case, number = 6, numberToAdd = 4, and numberToSubtract = 2.
+// CHAIN OF VARIABLES EXAMPLE:
+// first pay attention to the arguments in the call signtature.  These are "slots".  In this case, number = 6, numberToAdd = 4, and numberToSubtract = 2.
 //also, pay attention to how the variables are chained together and work off the previous one.
 
 function addSubtract(number, numberToAdd, numberToSubtract) {
@@ -134,15 +79,64 @@ function addSubtract(number, numberToAdd, numberToSubtract) {
         console.log(`and last, when we subtract ${numberToSubtract} from ${add}, we get ${subtract}`);
 }
 
-console.log(addSubtract(6,4,2))
+console.log(addSubtract(6,4,2))        //  first, we start with the number 6
+                                       //  second, when we add 6 to 4 we get 10
+                                       //  and last, when we subtract 2 from 10, we get 8
+                                       //  undefined
 
 
 
 
 
 
+// CHAIN OF VARIABLES v2 EXAMPLE:
+// the start being the initial number (i.e. the first "slot" in the call signature.
+// the second variable "mulitply" takes the initial number and multiplies that by the second "slot" in the call signature.
+// the third variable takes the second variable (i.e. multiply) and divides it by the third "slot" in the call signature.
+// last, we return divide.
+
+function multiplyDivide(number, numberToMultiply, numberToDivide) {
+    const start = number;
+    const multiply = start * numberToMultiply;
+    const divide = multiply / numberToDivide;
+    return divide;
+}
+
+console.log(multiplyDivide(2,5,2));    // 5
 
 
+
+
+
+
+// CHAIN OF VARIABLES W/ ANOTHER FUNCTION:
+// The FIRST variable (i.e. started) uses the first "slot" (i.e. number) which we assign the value of 1.
+// The SECOND variable (i.e. multiplied) passes the value of the variable "started" and multiplies it by the second slot (i.e. numberToMultiply) with the value 5.
+// The THIRD variable (i.e. divided) takes multiplied and divides by the third slot (i.e. numberToDivide).
+// The FOURTH variable uses an outside function to perform the last task which is to subtract 1 from the result of divide.
+//     -- To outside function is passed the result of divided as a parameter, which we can use to subtract from 1.
+// Lastly, when we return subtracted at the end of the function we draw on the subtract function
+
+function subtract(divide) {
+    return divide - 1;
+}
+
+function multiplyDivide2(number, numberToMultiply, numberToDivide) {
+    const started = number;
+    const multiplied = started * numberToMultiply;
+    const divided = multiplied / numberToDivide;
+    const subtracted = subtract(divided)
+    return subtracted;
+}
+
+console.log(multiplyDivide2(2,5,2));
+
+
+
+
+
+
+// 
 // Objective: Take a string, replace a word in that string, reverse that string using seperated functions
 
 function flip(string) {
@@ -154,12 +148,33 @@ function print(string) {
 }
 
 function replace(string) {
-     return string.replace("this","that");
+    return string.replace("this","that");
  }
 
 function replaceFlipPrint(string) {
     print(flip(replace(string)));
 }
+
+replaceFlipPrint("this is a tester string for replaceFlipPrint");
+
+
+
+
+
+
+
+
+function flip(string) {
+    return string.split('').reverse().join('');
+}
+
+function print(string) {
+    console.log(string);
+}
+
+function replace(string) {
+    return string.replace("this","that");
+ }
 
 function variablesReplaceFlipPrint(string) {
     let replaced = replace(string);
@@ -167,8 +182,17 @@ function variablesReplaceFlipPrint(string) {
     print(flipped); 
 }
 
-variablesReplaceFlipPrint("this is a tester string first");
-replaceFlipPrint("this is a tester string second");
+variablesReplaceFlipPrint("this is a tester string for variablesReplaceFlipString");
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -198,4 +222,30 @@ function replaceFlipPrint2(string) {
 
 console.log(replaceFlipPrint2("This is a tester 2"))
 
+
+
+
+
+// scratch
+
+// function collectData() {
+//     const numbers = [1,2,3,4,5];
+//     const string = "This is a mighty string";
+//     const putEmTogether = [...numbers] + [...string];
+//     console.log(putEmTogether);
+// }
+
+// console.log(collectData());
+
+// useful problem solving strategies
+
+// 1. Always have a plan
+// 2. Restate the problem
+// 3. Divide the Problem
+// 5. Start with what you know
+// 6. Reduce the Problem
+// 7. Look for Analogies
+// 8. Experiment
+// 9. Dont get frustrated
+// 10.
 
