73caa824e0ba3ecbefaf556ebb0aec1b60222c11
John Azzaro <jazzaro@temple.edu>
2018-12-30 10:35:00 -0800

    app logic initial commit

---
+++ appLogic.js
@@ -0,0 +1,47 @@
+"use strict";
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. 
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the problem?:  
+//    1. 
+
+// What is the BEST solution? 
+//     1. 
+
+// What are the special components of these solutions?:  
+//     1. 
+
+// What needs work?
+//     1. 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// ABOUT APPLICATION, CONTROL FLOW, AND CONDITIONAL LOGIC...
+// 1. There are three essential logical operators: &&, ||, and !.
+//    
+//       && -- "and" evaluates as true if both values are true.
+//              for example: 
+//                           const foo = true
+//                           const bar = false
+//                           const ree = true
+//                           
+//                           foo && bar;     // false
+//                           foo && ree;     // true
+//
+//       || -- "or" evaluates as true if one of the values evaluates as true.
+//              for example:                             
+//                           const foo = true
+//                           const bar = false
+//                           const ree = true
+//
+//                           foo || bar       // true
+//                           foo || ree       // false
+//
+//       !  -- "exclamation" negates a boolean value.
+//              for example:
+//                           const foo = true
+//     
+//                           foo;             // true
+//                           !foo;            // false
