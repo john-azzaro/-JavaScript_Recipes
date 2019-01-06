aaa06763e2eb9388b4cbc899e68637afc4ce7cb2
John Azzaro <jazzaro@temple.edu>
2019-01-05 21:42:14 -0800

    initial commit

---
+++ anagram.js
@@ -0,0 +1,26 @@
+"use strict";
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. 
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the problem?:  
+//    1. Check to see if two provided strings are anagrams of each other.
+//       One string is an anagram of another if it uses the same chatacters in the same quantity.
+//       Only consider characters, not spaces or punctuation.
+//       Capital letters are considered the same as lower case.
+//
+//       examples
+//       anagrams('rail safety', 'fairy tales') --> true
+//       anagrams('RAIL! SAFETY!', 'fairy tales') --> true
+//       anagrams('Hi there', 'Bye there')  --> false
+
+// What is the BEST solution? 
+//     1. 
+
+// What are the special components of these solutions?:  
+//     1. 
+
+// What needs work?
+//     1. 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
