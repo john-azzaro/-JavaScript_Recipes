6bb3f762faef57bceca72fc0398c48beccc37429
John Azzaro <jazzaro@temple.edu>
2019-01-27 21:19:47 -0800

    added summary

--- appStructure.js
+++ appStructure.js
@@ -1,2 +1,18 @@
 'use strict';
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//    1. Provide a showcase of different ways to structure an app using jQuery and other technologies.
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the problem?:  
+//    1. There is a critical lack of application structure examples to learn from, so this document attempts to 
+//       provide a few interesting working patterns to build from.
+
+// What is the BEST solution? 
+//     1. So far the best solution is to make sure that every function has a single concern and that all necessarily flow 
+//        from each other.
+
+// What are the special components of these solutions?:  
+//     1. jQuery
+
+// What needs work?
+//     1. 
 
