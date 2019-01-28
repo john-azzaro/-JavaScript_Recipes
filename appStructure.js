f8b971ce6217df0b66596abc0f6ad75933061258
John Azzaro <jazzaro@temple.edu>
2019-01-27 21:35:21 -0800

    more notes

--- appStructure.js
+++ appStructure.js
@@ -32,13 +32,44 @@
     5. display header, section, and footer.
     6. generate header, section, and footer.
 
-    
-
-
-
-
-
-
-
-
 */
+
+// INITIAL CODING ///////////////////////////////////////////////////////////////
+// In the HTML document, be sure to first write out the strucutre of your app first so that when the time
+// comes, you will be able to insert the sections into your generate functions.
+
+
+// IN HTML //////////////////////////////////////////////////////////////////////
+// important to just note that the main section delegates the document.
+/* 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+     <main class="grid" aria-live="assertive"></main> 
+    
+     <script src="index.js"></script>
+</body>
+</html>
+*/
+
+
+// IN JS ///////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
