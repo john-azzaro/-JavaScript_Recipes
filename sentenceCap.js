a47f6102b3cca82bb3a4610da9728cfb1d75c5cd
John Azzaro <jazzaro@temple.edu>
2019-02-02 20:55:20 -0800

    summary details

--- sentenceCap.js
+++ sentenceCap.js
@@ -5,7 +5,11 @@
 //     1. 
 // SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
 // What is the problem?:  
-//    1. 
+//    1. Write a funciton that accepts a string.  The function should capitalize the first letter in each word
+//       in the string and return the capitalized string.
+//
+//       example: capitalize('a short sentence')  =>  A Short Sentence
+//                capitalize('a lazy fox')        =>  A Lazy Fox
 
 // What is the BEST solution? 
 //     1. 
@@ -18,3 +22,10 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/* 
+
+
+
+
+
+*/
