517b66b94da6277056d68c30aeefd14162ae53d5
John Azzaro <jazzaro@temple.edu>
2019-02-10 19:59:41 -0800

    solution 1 and problem summary

--- stepsQuest.js
+++ stepsQuest.js
@@ -4,7 +4,21 @@
 //     1. 
 // SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
 // What is the problem?:  
-//    1. 
+//    1. write a function that accepts a positive number N. the function should console log a step shape with N levels using the # character.
+//       make sure that the step has spaces on the right side.
+//       example:
+//                 steps(2)
+//                     '# '
+//                     '##'
+
+//                 steps(4)
+//                     '#   '
+//                     '##  '
+//                     '### '
+//                     '####'       
+//             
+//       * so in the example above, when we pass an argument (integer), we'll see two lines
+//       * in the example with 4 steps, also note the spaces between the pound and the quote.
 
 // What is the BEST solution? 
 //     1. 
@@ -17,3 +31,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+function steps(n) {
+    
+}
