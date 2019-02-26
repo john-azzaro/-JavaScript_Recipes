ab0b60a3f8cda899fe9c412778494b5dd94ab4d9
John Azzaro <jazzaro@temple.edu>
2019-02-25 20:15:03 -0800

    initial commit

---
+++ pyramidQues.js
@@ -0,0 +1,39 @@
+"use strict";
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. write a function that accepts a positive number N.  The function should console lof a pyramid style
+//        shape with N levels using the # character.  Make sure the pyramid has spacies on both the left and right sides.
+//
+//        Example 1:
+//                   pyramid(1)
+//                          '#'
+
+//                   pyramid(2)
+//                         " # "
+//                         "###"
+
+//                   pyramid(3)
+//                         "  #  "
+//                         " ### "
+//                         "#####"
+//                       
+
+
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the problem?:  
+//    1. 
+
+// What is the BEST solution? 
+//     1. 
+
+// What are the special components of these solutions?:  
+//     1. 
+
+// What needs work?
+//     1. 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Note that this is related to the steps question
+
+*/
