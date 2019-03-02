312a4b8f54224142844651f3669d2af167eed035
John Azzaro <jazzaro@temple.edu>
2019-03-01 21:28:34 -0800

    solution 1.2.2 with regex and .match method

--- findVowels.js
+++ findVowels.js
@@ -14,9 +14,10 @@
 // What are the special components of these solutions?:  
 //     1. for...of loop
 //     2. .includes() -- 
+//     3. .match -- used to see if some possible thing (in the parentheses) is included in the string.
 
 // What needs work?
-//     1. 
+//     1. solution 1.2.2 has some difficulty because VSC doesnt seem to recognize regexpressions.  need to find out more about this.
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -82,10 +83,20 @@ console.log(vowels2("hello there again"));   // 7
 
 ///// SOlUTION 1.2.2 ///////////////////////////////////////
 // with this solution, which should be a bit cleaner than the one above, we'll use a regular expression and the method .match().
+ // this is saying that if this string has any character that is inside the brackets ([]), then let us know.  
+ // then we add two options g and i.  g makes sure we dont stop at the first match.  the i (insensitive) or case sensitive, takes care of cases for us.
+ // now with the match function, if it finds an array of all the matches that were found, so we return is assign it to a varaible called matches.
+ // but now if there is a mathc, it will either be an array (if vowel found) or null.
+ // to handle this, we need a ternary expression.
+ // in the ternary expression, we look at the matches variable.  if matches is null (false value), if an array (TRUE).
+ // so for the first part of the ternary, IF matches is a truthy value, then I want to return matches.length.
+ // otherwise if matches is null, then 0 (i.e. : 0).
 
 
 function vowels3(str) {
-
+    const matches = str.match(/[aeiou]/);  
+    return matches ? matches.length : 0;            
 }
 
-console.log(vowels3("hello there for the third time"));   // 7
+console.log("solution 1.2.2 using regular expression and .match");
+console.log(vowels3("hello there for the third time"));   
