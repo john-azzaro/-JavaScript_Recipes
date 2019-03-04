c731a8fe9c68d8af4211d41aa5e3b1b9e3cb9e64
John Azzaro <jazzaro@temple.edu>
2019-03-03 20:33:09 -0800

    added problem synopsis

--- GenMatrixSpirals.js
+++ GenMatrixSpirals.js
@@ -1,10 +1,21 @@
 "use strict";
 // OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
-//     1.                       
+//     1. Write a function that accepts an integer N and returns a NxN spiral matrix.
+//
+//     Example:   matrix(2)
+//                  [ [1,2],
+//                    [4,3] ]   
+
+//    Example: matrix(3)
+//                 [ [1, 2, 3],
+//                   [8, 9, 4],
+//                   [7, 6, 5] ]
 
 // SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
 // What is the problem?:  
-//    1. 
+//    1. When we call the function, we want to return a two dimensional array (an array of arrays).
+//       The reason this is called a spiral matrix is becasue we are goign to start at one and then go around clockwise.
+//       This had proven to be a VERY difficult problem and requires a LOT of thought.
 
 // What is the BEST solution? 
 //     1. 
