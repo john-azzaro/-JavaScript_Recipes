52dc1e6607d40e7f6ca553a70ce20580744603de
John Azzaro <jazzaro@temple.edu>
2019-03-18 22:14:21 -0700

    intitial commit with objectives

---
+++ FibonacciSeries.js
@@ -0,0 +1,30 @@
+"use strict";
+// OBJECTIVE ////////////////////////////////////////////////////////////////////////////////////////////////
+//     1. Print out the n-th entry (i.e. the n in >> function fib(n) {...}) << in the fibonacci series.
+//        The fibonacci series is an ordering of numbers where each number is the sum of the preceding two.
+//         For example, the sequence [0,1,1,2,3,5,8,13,21,34] forms the first ten entries of the fibonacci series.
+
+//         Example: fib(4) === 3
+
+// SUMMARY //////////////////////////////////////////////////////////////////////////////////////////////////
+// What is the problem?:  
+//    1. Profile differences in runtime complextiy between different solutions
+
+// What is the BEST solution? 
+//     1. 
+
+// What are the special components of these solutions?:  
+//     1. 
+
+// What needs work?
+//     1. 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Basically with a fibonacci sequence, you take the first two numbers, add them together, and then you get a new number... then repeat.
+
+
+
+
+*/
