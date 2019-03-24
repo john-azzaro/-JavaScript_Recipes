8338ba07ec9be3f6c7cf5ed31ffcd50e2e8c070b
John Azzaro <jazzaro@temple.edu>
2019-03-23 21:21:20 -0700

    variable basics

--- Variables&DataStruc.js
+++ Variables&DataStruc.js
@@ -4,6 +4,27 @@
 //     2. Data Types
 
 // TOPIC NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. Useful overview of information on  on variables and data types taken from study, research, tutorials, 
+//        mentor meetings, peer discussions, and good ole' fashioned curiosity.  
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
+
+What is a variable?
+    - a variable is a name attached to a variable.
+    - a variable stores and keeps track of information within a program.
+    - a variable manages the state of a program.
+        -- i.e. let clickCount = 2.
+    - a variable uses the equal sign (=) to assign a value to a variable.
+        -- i.e let name = 'john'
+    
+
+What is the basic structure of a variable?
+    JavaScript Keyword      Variable Name     Assinment Operator        Value
+    __________________      _____________     __________________      ________
+         let                    name                  =                'john'
+
+
+
+*/
+
