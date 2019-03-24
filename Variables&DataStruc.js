101a8e4c323bc3b6d7494e9fe2ab1e8039aec15b
John Azzaro <jazzaro@temple.edu>
2019-03-23 21:33:48 -0700

    data types

--- Variables&DataStruc.js
+++ Variables&DataStruc.js
@@ -2,6 +2,7 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Variables 
 //     2. Data Types
+//
 
 // TOPIC NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on  on variables and data types taken from study, research, tutorials, 
@@ -23,6 +24,45 @@ What is the basic structure of a variable?
     JavaScript Keyword      Variable Name     Assinment Operator        Value
     __________________      _____________     __________________      ________
          let                    name                  =                'john'
+
+
+What are the THREE Commands used to create variable?
+    var    -- value can be changed.
+    let    -- value can be changed.
+    const  -- value CANNOT be changed.
+
+    For example: 
+    */
+        var redFruit = 'apple';
+        let yellowFruit = 'banana';
+        const greenFruit = 'kiwi'
+    /* 
+
+
+What is a Data Type?
+    - A data type is a kind of value that is assigned to a variable.
+    - there are 6 (or really 7) data types.
+
+    1. string     -     A string is a series of characters within single or double quotes.
+                        -- 'Hello my name is Bill'
+                        -- "How now brown cow"
+
+    2. number     -     A number is used to represent numbers, both integers an floating point decimals numbers.
+                        -- 23
+                        -- 1.387
+
+    3. Boolean    -     A Boolean evaluates either true or false.
+                        -- true
+                        -- false
+
+    4. Null       -     Null is a special value used to indicate that the variable has no value.
+                        -- null
+
+    5. Undedined  -     Special value that declares variable before assigning a value to them.
+                        Assigned by the browser when first created in memory.
+                        
+    6. 
+
 
 
 
