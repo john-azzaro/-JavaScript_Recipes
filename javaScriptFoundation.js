8e6679ef583aedbdd6bbf6a363c8061f87895c34
John Azzaro <jazzaro@temple.edu>
2019-03-27 14:52:58 -0700

    programming and JavaScript

--- javaScriptFoundation.js
+++ javaScriptFoundation.js
@@ -2,7 +2,8 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Foundations of JavaScript
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. What is a program?
+//     2. What is JavaScript?
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on establishing the foundations of JavaScript from study, research, tutorials, 
@@ -10,3 +11,22 @@
 //        and Answer format for improved readability.
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+1. What is a program?
+////////////////////
+    - a program is a set of instructions a computer carries out.
+    - the skill of a programmer is finding a solution to a problem and implementing those solutions with a programming language
+      so the computer can execute it.
+*/
+
+/*
+2. What is JavaScript?
+/////////////////////
+    - JavaScript is the only lnaguage that can be used on all modern browsers.
+    - JavaScript is specified by ECMA and W3C specs.
+    - JavaScript relies on individual browsers to handle how to implement ECMA and W3C specifications. 
+    - JavaScript is used in two ways:
+        -- First, modeling and manipulating data and proceses.
+        -- Second, interacting with browser elements.
+*/
