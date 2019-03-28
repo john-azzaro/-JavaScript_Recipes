302b6785a751ad6fc2bd052c5f4be9c4103c83d7
John Azzaro <jazzaro@temple.edu>
2019-03-28 10:18:45 -0700

    overview of fundamental operators

--- applicationLogic.js
+++ applicationLogic.js
@@ -3,8 +3,9 @@
 //     Application Logic
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. What are logical assertions?
-//     2. What are the 3 logical assertions?
-//     3. What is the difference between strict and loose equality?
+//     2. What are operators?
+//     3. What are the 3 logical assertions?
+//     4. What is the difference between strict and loose equality?
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful information about application logic from study, research, tutorials, 
@@ -18,6 +19,24 @@
 //////////////////////////////
     - ASSERTIONS are statements that evaluates to either TRUE or FALSE.
     - LOGICAL ASSERTIONS allow us to write conditional logic in JavaScript
+*/
+
+/*
+2. What are operators?
+/////////////////////
+     - OPERATORS are used along with variables and constants to create EXPRESSIONS.
+     - EXPRESSIONS can implement logic and algorithms.
+     - There are 5 operators in JavaScript:
+                -- Arithmatic 
+                -- Assignment 
+                -- Comparison 
+                -- Logical 
+                -- Bitwise 
+
+
+
+
+
 */
 
 /*
