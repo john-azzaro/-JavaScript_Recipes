cb1b4212e12cdf458a73727cbd0457e36bfab9e7
John Azzaro <jazzaro@temple.edu>
2019-03-30 22:43:13 -0700

    issues with infinite loops

--- controlFlow.js
+++ controlFlow.js
@@ -121,6 +121,43 @@ What is a switch...case statement?
                     console.log(role("Tom"));
 
 
+/*
+8. What is a ternary operator?
+//////////////////////////////
+        - A TERNARY OPERATOR is short-cut conditional statement.
+        - A ternary operator is the only JavaScript operator that takes three operands. 
+        - In the example below, we want to determine whether a person can drink (or not).
+        - Also note that although you dont need the parentheses fro the logical expression, it does help in visual identification.
+
+                --    let canIDrink        -- is the keyword and variable name.
+                --     (age >= 21)         -- This is the logical expression, specifically IF age is greater or equal to 21...
+                --          ?              -- If this condition is true...
+                -- 'Yes, you can drink'    -- Return this.
+                --          :              -- If the condition is false...
+                -- 'No, you cannot drink'  -- The value returned if the condition is false.
+*/
+
+                let age = 21;
+                let canIDrink = (age >= 21) ? 'Yes, you can drink' : 'No, you cannot drink';    
+                console.log(canIDrink);   //-> Yes, you can drink
+/* 
+                - Additionally, a ternary operator is right-associative, so conditions can be chained together.
+                - note that the format can be a single line or multiple lines depending on your format preference.
+*/
+                let direction = 'right'
+                let myDirection = (direction == 'left') ? 'wrong direction' : (direction == 'right') ? 'right!' : 'where are you going?';
+                console.log(myDirection);
+
+                //or//
+
+                // let myNumber = 4;
+                // let result = (myNumber == 1) ? "a" 
+                //         : (myNumber == 2) ? "b" 
+                //         : (myNumber == 3) ? "c" 
+                //         : (myNumber == 4) ? "d" 
+                //         : "e"; 
+                //         console.log(result));
+
 
 /*
 What is iteration (aka loop)?
@@ -272,41 +309,23 @@ What is a do...while loop?
         console.log(doWhileLoop());
 
 
-
-
 /*
-8. What is a ternary operator?
-//////////////////////////////
-        - A TERNARY OPERATOR is short-cut conditional statement.
-        - A ternary operator is the only JavaScript operator that takes three operands. 
-        - In the example below, we want to determine whether a person can drink (or not).
-        - Also note that although you dont need the parentheses fro the logical expression, it does help in visual identification.
+What is an infinite loop?
+//////////////////////////
+        - an infinite loop execture indefinitly.
+        - an infinite loop is executed by simply forgetting to add the i++ incrementation.
 
-                --    let canIDrink        -- is the keyword and variable name.
-                --     (age >= 21)         -- This is the logical expression, specifically IF age is greater or equal to 21...
-                --          ?              -- If this condition is true...
-                -- 'Yes, you can drink'    -- Return this.
-                --          :              -- If the condition is false...
-                -- 'No, you cannot drink'  -- The value returned if the condition is false.
+        function infiniteLoop() {
+            let i = 0;             
+            while (i <= 5) {       
+                console.log(i);       // note the omission of the i++ at the end of the while code block. This will crash your editor.              
+            }
+        }
+
 */
 
-                let age = 21;
-                let canIDrink = (age >= 21) ? 'Yes, you can drink' : 'No, you cannot drink';    
-                console.log(canIDrink);   //-> Yes, you can drink
-/* 
-        - Additionally, a ternary operator is right-associative, so conditions can be chained together.
-        - note that the format can be a single line or multiple lines depending on your format preference.
-*/
-                let direction = 'right'
-                let myDirection = (direction == 'left') ? 'wrong direction' : (direction == 'right') ? 'right!' : 'where are you going?';
-                console.log(myDirection);
 
-                //or//
 
-                // let myNumber = 4;
-                // let result = (myNumber == 1) ? "a" 
-                //         : (myNumber == 2) ? "b" 
-                //         : (myNumber == 3) ? "c" 
-                //         : (myNumber == 4) ? "d" 
-                //         : "e"; 
-                //         console.log(result));
+
+
+
