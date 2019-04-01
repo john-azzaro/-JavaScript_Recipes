a54d8cb2cae4c6d13dc3bf310bd053cb15b6eecb
John Azzaro <jazzaro@temple.edu>
2019-04-01 13:33:03 -0700

    object components

--- objects.js
+++ objects.js
@@ -2,7 +2,9 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Objects
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. What is an Object?
+//     2. What are the components of an object?
+//     3. What are object methods?
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on objects taken from study, research, tutorials, 
@@ -10,3 +12,37 @@
 //        and Answer format for improved readability.
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+What is an object?
+//////////////////
+    - objects are complex datat types that allow you to bring together common properties and behaviors into a single entity.
+    - objects provide an excellent way of organizing code that belongs together.
+    - objects helpyou avoid global variables.
+    - objects let us represent instances of some model.
+    - objects are data structures that hold key:value pairs.
+        -- think of key value pairs like words in a dictionary, with a word (i.e. key) and its definition (i.e. value).
+
+*/
+
+/*
+What are the components of an object?
+/////////////////////////////////////
+    - an OBJECT LITERAL is a comma seperated list of name/value pairs wrapped inside curly braces.
+    - inside the object literal are key/value pairs.
+    - each key/value pair has a name followed by a colon and a value followed by a comma.
+    - values that are strings must be enclosed by quotation marks.
+    - values can be any data type.
+    - if you need a space or a period in a key, you need to use quotation marks. (i.e. 'first hobbit': 'frodo',).   
+*/
+
+//      variable   object name        object literal
+//             \          |            /          
+                const theFellowship = {
+                    wizard: 'gandalf',        //- key: value,  (note the comma that seperates each key/value pair).
+                    man: 'Aragon',
+                    elf: 'Legolas',
+                    dwarf: 'gimli',
+                    hobbit: 'frodo'
+                }
