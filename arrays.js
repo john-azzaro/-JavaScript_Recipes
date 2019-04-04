a8eb3a44fe0151555f3b372997e898d0f810ee68
John Azzaro <jazzaro@temple.edu>
2019-04-04 16:18:08 -0700

    adding elements to an array

--- arrays.js
+++ arrays.js
@@ -31,6 +31,10 @@
     -- To create an array, simply declare a variable and assign empty brackets to it. (i.e. []).
     -- Arrays can be created with 0 or more elements.    
     -- Items in the array are seperated by commas (i.e. ,).
+    -- if you use `const` to instantiate the variable, know that you CANNOT reassign 
+       data types to something else (i.e. number to data type).
+        -- however, it is possible to MODIFY the content of the array (i.e. change a number 
+           to another number), add or remove existing elements.
 */
     // How to create an empty array:
     //===============================
@@ -60,7 +64,8 @@
 ///////////////////////////////////////
     -- To access items in array, you need to acess by INDEX.
     -- The index of an item is its position in an array.
-    -- Item index are ZERO INDEXED, meaning the count starts at 0 and continues on for as many items as there are in the array.
+    -- Item index are ZERO-INDEXED, meaning the count starts at 0 and continues on for as many 
+       items as there are in the array.
 */
     //            Index number:     0         1        2        3        4                
     //                              |         |        |        |        |
@@ -89,5 +94,95 @@
 /*
 5. What is an array method?
 ///////////////////////////
-    -- an array method
+    -- an array method traverses and manipulates the array and the items.
+    -- since an array is an object, we can use both bracket AND dot-notation.
 */
+
+
+
+/*
+6. How do you add elements to an array?
+///////////////////////////////////////
+    -- in an array, you can add elements to the:
+        -- beginning (.unshift)
+        -- middle (.)
+        -- end (.push)
+*/
+
+/*
+   How do you add elements to the BEGINNING?
+   =========================================
+    -- to add elements to the beginning of an array, you use the .unshift method.
+*/  
+            const addToBeginning = ['apple', 'banana', 'carrot']; 
+
+            console.log(`==> add elements to beginning with .unshift`);
+
+            console.log(addToBeginning);        //-> [ 'apple', 'banana', 'carrot' ]
+            addToBeginning.unshift('avocado');
+            console.log(addToBeginning);        //-> [ 'avocado', 'apple', 'banana', 'carrot' ]
+
+
+/*
+   How do you add elements to the MIDDLE?
+   ======================================
+    -- to add elements to the middle of an array, you use the .splice method.
+    -- using .splice, first consider the array you are inserting your element into:
+
+                                    0         1         2
+                                    |         |         |
+            const addToMiddle = ['apple', 'banana', 'carrot']; 
+
+
+    -- there are 3 elements in the array.  
+    -- Now suppose we want to insert the element 'kiwi' and 'orange' between 'banana' and 'carrot'.
+    -- we use dot-notation first with the name of the array, then .splice, then a few critical parameters:
+
+        -- parameter 1 = index of where we want to begin new element insertion.
+        -- parameter 2 = delete count of what you want to remove.  We dont want to delete 
+                         anything, so we use 0.
+        -- parameter 3 = element(s) you want to add.  You can add as many elements after if you want.
+
+
+                insertion start       first element to insert at index of 2
+                              \         |
+            addToMiddle.splice(2, 0, 'kiwi', 'orange');
+                                   \               \  
+                              delete count        second element to insert at index of 3
+
+
+        -- in the .splice method above, we are saying that we want to insert at index of 2 (i.e. the element 
+           you insert will be where the element 'carrot' currently is (i.e. 'carrot' = index of 2), while not 
+           removing anything (i.e. 0), the elements 'kiwi' and 'orange'.
+*/  
+
+            const addToMiddle = ['apple', 'banana', 'carrot']; 
+
+            console.log(`==> add elements to middle with .splice`);
+
+            console.log(addToMiddle);                      //-> [ 'apple', 'banana', 'carrot' ]
+            addToMiddle.splice(2, 0, 'kiwi', 'orange');
+            console.log(addToMiddle);                      //-> [ 'apple', 'banana', 'kiwi', 'orange', 'carrot' ]
+
+
+/*
+   How do you add elements to the END?
+   ===================================
+    -- to add elements to the end of an array, you use the .push method.
+*/  
+            const addToEnd = ['apple', 'banana', 'carrot']; 
+
+            console.log(`==> add elements to end with .push`);
+
+            console.log(addToEnd);    //-> [ 'apple', 'banana', 'carrot' ]
+            addToEnd.push('date');
+            console.log(addToEnd);    //-> [ 'apple', 'banana', 'carrot', 'date' ]
+
+
+
+
+
+
+
+
+
