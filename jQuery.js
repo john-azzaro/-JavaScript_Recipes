b045a3b8456dca6327e5d1d8587902a5bc77015e
John Azzaro <jazzaro@temple.edu>
2019-04-09 21:18:37 -0700

    Document Object Model

--- jQuery.js
+++ jQuery.js
@@ -2,7 +2,7 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     jQuery
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. What is the DOM?
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on jQuery taken from study, research, tutorials, 
@@ -10,3 +10,15 @@
 //        and Answer format for improved readability.
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+1. What is the DOM?
+    -- DOM is short for 'Document Object Model'.
+    -- DOM is the CURRENT REPRESENTATION of the CURRENT STATE of the HTML content on the page.
+    -- DOM specifies how JavaScript can access and update the content of a web page.
+    -- DOM is a cross-platform and language independent model for representing and interacting with elements in HTML documents.
+    -- DOM is NOT part of HTML.
+    -- DOM is NOT part of JavaScript
+
+*/
