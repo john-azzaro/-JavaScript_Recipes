6e5015fa7fa33bc718b6e50448db22febcddb047
John Azzaro <jazzaro@temple.edu>
2019-04-09 21:29:14 -0700

    DOM document notes

--- jQuery.js
+++ jQuery.js
@@ -18,7 +18,37 @@
     -- DOM is the CURRENT REPRESENTATION of the CURRENT STATE of the HTML content on the page.
     -- DOM specifies how JavaScript can access and update the content of a web page.
     -- DOM is a cross-platform and language independent model for representing and interacting with elements in HTML documents.
-    -- DOM is NOT part of HTML.
-    -- DOM is NOT part of JavaScript
+    -- DOM is NOT part of HTML and it is NOT part of JavaScript... it is a seperate set of rules.
+    -- DOM CANNOT work without a DOCUMENT.
+    -- DOM is NOT to be confused with the HTML file of the page
+        -- the HTML file will show what the page will look like INITIALLY.
+        -- However, as JavaScript begins to take over elements can be added or removed regardless of whether they were orginally
+           in the HTML document.
+    -- When you create a web page and load it in a web browser the DOM is ENABLED.
+    -- Think of the DOCUMENT as a tree.
+        -- the tree describes the relationship between its members like parent, child, sibling, etc.
 
-*/
+                             <title>
+                            /
+                      <head> - <meta>
+                     /      
+                <html>
+                     \
+                      <body> - <main>
+                           \
+                            <nav>
+
+    -- The DOCUMENT begins by opening the HTML tag 
+        -- This would be a PARENT which all the other elements are contained within.
+        -- HTML tag has no parent or siblings.
+    -- One level deepr, we have two elements: head and body.
+        -- the head and the body are siblings.
+
+    --
+
+
+
+
+
+
+*/  
