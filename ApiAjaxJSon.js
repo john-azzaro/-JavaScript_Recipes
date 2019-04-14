f8f37ad669fc79e21df999e88c85caee64f7bc94
John Azzaro <jazzaro@temple.edu>
2019-04-14 14:58:40 -0700

    what is an api

--- ApiAjaxJSon.js
+++ ApiAjaxJSon.js
@@ -2,7 +2,7 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     API's, AJAX, JSON with jQuery
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. What is an API?
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information on creating apps with jquery, including API's, AJAX, and jQuery taken from 
@@ -10,3 +10,40 @@
 //         document in Question and Answer format for improved readability.
 //        
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+1. How is the difference between static and 
+*/
+
+/*
+What is an API?
+///////////////
+    -- API stands for Application Programming Interface.    
+    -- An API is a specification allowing two systems to communicate.
+        -- More simply put, an API allows two strangers to speak to each other so that we can share data and work with each other.
+        -- for example, the jQuery library provides an API for DOM traversal and manipulation, like .find, $(), etc.
+    -- An API is a messenger that takes a request and tells the system what to do. 
+    -- Example 1: Expedia
+        -- For example, if you look at Expedia and try to findfrom San Diego to New York, it will search all websites
+           and airlines for the best deals.  
+        -- So how is this one website able to find flights from multiple websites?
+            -- The website iteself doesnt have that information itself... that information is in the data base of the airlines themselves.
+            -- The airline is interested in having thier flights on as many websites as possible to sell as many tickets as possible.
+        -- Expedia has all this information by using the API's
+            -- Now Expedia doesnt have access to the databases of all the airlines.
+            -- BUT Expedia DOES have 
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+/*
+UdThComWeDevin2019Ze2MasApis
+*/
