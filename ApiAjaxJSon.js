ce8768a48af05906e07aa1ee4023d067087a0420
John Azzaro <jazzaro@temple.edu>
2019-04-21 21:19:01 -0700

    github API example

--- ApiAjaxJSon.js
+++ ApiAjaxJSon.js
@@ -519,6 +519,45 @@ X. What is an API endpoint and query string?
 ///////////////////////////////////////////
     -- An API endpoint is an interface the consisting of one or more publicly exposed ENDPOINTS to a defined request-response message
        system (i.e. JSON) which is exposed via the web by means of an HTTP-based web server.
+
+    EXAMPLE 1: GitHub API
+    =====================
+    -- Suppose you want to make an app that uses the information contained in the GitHub API.
+    -- Go to the GitHub API address: https://api.github.com/
+    -- What you will see is a variety of endpoints available to you:
+
+            current_user_url	"https://api.github.com/user"
+            current_user_authorizations_html_url	"https://github.com/settings/connections/applications{/client_id}"
+            authorizations_url	"https://api.github.com/authorizations"
+            code_search_url	"https://api.github.com/search/code?q={query}{&page,per_page,sort,order}"
+            commit_search_url	"https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}"
+            emails_url	"https://api.github.com/user/emails"
+            emojis_url	"https://api.github.com/emojis"
+            events_url	"https://api.github.com/events"
+            feeds_url	"https://api.github.com/feeds"
+            followers_url	"https://api.github.com/user/followers"
+            following_url	"https://api.github.com/user/following{/target}"
+            gists_url	"https://api.github.com/gists{/gist_id}"
+            hub_url	"https://api.github.com/hub"
+            issue_search_url	"https://api.github.com/search/issues?q={query}{&page,per_page,sort,order}"
+            issues_url	"https://api.github.com/issues"
+            keys_url	"https://api.github.com/user/keys"
+            notifications_url	"https://api.github.com/notifications"
+            organization_repositories_url	"https://api.github.com/orgs/{org}/repos{?type,page,per_page,sort}"
+            organization_url	"https://api.github.com/orgs/{org}"
+            public_gists_url	"https://api.github.com/gists/public"
+            rate_limit_url	"https://api.github.com/rate_limit"
+            repository_url	"https://api.github.com/repos/{owner}/{repo}"
+            repository_search_url	"https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}"
+            current_user_repositories_url	"https://api.github.com/user/repos{?type,page,per_page,sort}"
+            starred_url	"https://api.github.com/user/starred{/owner}{/repo}"
+            starred_gists_url	"https://api.github.com/gists/starred"
+            team_url	"https://api.github.com/teams"
+            user_url	"https://api.github.com/users/{user}"
+            user_organizations_url	"https://api.github.com/user/orgs"
+            user_repositories_url	"https://api.github.com/users/{user}/repos{?type,page,per_page,sort}"
+            user_search_url	"https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
+
 */
 
 
