381be1c6fb71efba5309aaa3d48b73e02deddc02
John Azzaro <jazzaro@temple.edu>
2019-04-23 22:23:39 -0700

    AJAX and functions

--- ApiAjaxJSon.js
+++ ApiAjaxJSon.js
@@ -641,6 +641,21 @@ function getDataFromApi(searchTerm, callback) {
   $.getJSON(GITHUB_SEARCH_URL, query, callback);
 }
 
+function renderResult(result) {
+    return `
+      <div>
+        <h2>
+        <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+        <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
+        <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+      </div>
+    `;
+  }
+  
+  function displayGitHubSearchData(data) {
+    const results = data.items.map((item) => renderResult(item));
+    $('.js-search-results').html(results);
+  }
 
 
 function handleSubmit() {
