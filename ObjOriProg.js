d314a998f50a71dc9504b3ef88c16b56c6a7c686
John Azzaro <jazzaro@temple.edu>
2019-04-28 17:38:15 -0700

    what is OOp

--- ObjOriProg.js
+++ ObjOriProg.js
@@ -2,7 +2,7 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Object-oriented Programming
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. 
+//     1. What is Object Oriented Programming (OOP)? 
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information about Object-oriented programming (OOP) taken from study, research,  
@@ -10,3 +10,16 @@
 //        document in Question and Answer format for improved readability.
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+1. What is Object Oriented Programming (OOP)?
+/////////////////////////////////////////////
+    -- Object oriented programing (OOP) is a programming paradigm centered around objects rather than functions.
+    -- Object oriented programming is not a programming language or tool.
+    -- Languages such as C#, Java, Ruby, Python, and JavaScript, and more.
+    -- Many frameworks are designed with object oriented programming concepts in mind (e.g. angular(.
+
+
+
+*/
