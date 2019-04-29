131313dc860e610ba0793028c816e09541a4717f
John Azzaro <jazzaro@temple.edu>
2019-04-28 17:40:16 -0700

    4 concepts outline

--- ObjOriProg.js
+++ ObjOriProg.js
@@ -2,7 +2,8 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Object-oriented Programming
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. What is Object Oriented Programming (OOP)? 
+//     1. What is Object Oriented Programming (OOP)?
+//     2. What are the four core concepts in OOP? 
 //
 // NOTES ///////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful overview of information about Object-oriented programming (OOP) taken from study, research,  
@@ -19,7 +20,15 @@
     -- Object oriented programming is not a programming language or tool.
     -- Languages such as C#, Java, Ruby, Python, and JavaScript, and more.
     -- Many frameworks are designed with object oriented programming concepts in mind (e.g. angular(.
+*/
 
-
+/*
+2. What are the four core concepts of OOP?
+//////////////////////////////////////////
+    -- 
+        1. Excapsulation
+        2. Abstraction
+        3. Inheritance 
+        4. Polymorphism
 
 */
