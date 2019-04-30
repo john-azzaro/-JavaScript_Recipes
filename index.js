e37dd49800dc15f2a714e2a31e5f86017ef0f275
John Azzaro <jazzaro@temple.edu>
2019-04-30 11:50:42 -0700

    ISSUE: private properties w/ example

--- index.js
+++ index.js
@@ -10,3 +10,48 @@
 //        tutorials, mentor meetings, peer discussions, and good ole' fashioned curiosity. 
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+TOPICS AND QUESTIONS FOR FURTHER IDEA
+//////////////////////////////////////
+
+    ISSUE: Need to get a better idea (and example) of how private properties and methods work.
+            -- current work location: objectOriProg  > Private Properties and Methods,
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
