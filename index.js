8b2efa88a1b5017eeb64816de98e8a73715c6384
John Azzaro <jazzaro@temple.edu>
2019-04-30 11:59:07 -0700

    create issueList, move contents, clear scratch

--- index.js
+++ index.js
@@ -2,37 +2,14 @@
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //     Scratch pad for JavaScript Recipes
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//     1. Issue List
-//        + Open Issues
-//        + Resolved Issues
+//     1. 
 //
 // NOTES ////////////////////////////////////////////////////////////////////////////////////////////////////
 //     1. Useful scratch pad about everything programming and additional questions  from study, research,  
 //        tutorials, mentor meetings, peer discussions, and good ole' fashioned curiosity. 
-//     2. Note on Issue List:  Questions, concerns, or even anything that looks interesting goes in the OPEN
-//        section as well as location in files.  Once the question is resolved, add RESOLUTION notes.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-
-
-ISSUE LIST
-////////////////
-
-///// OPEN /////
-
-    ISSUE: Need to get a better idea (and example) of how private properties and methods work.
-            -- current work location: objectOriProg  > Private Properties and Methods,
-
-       
-
-
-
-
-
-///// RESOLVED /////
-
-
 
 
 
