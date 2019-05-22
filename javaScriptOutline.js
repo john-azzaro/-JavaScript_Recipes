7df57f266e378d749657f2227c230dd2796e690e
John Azzaro <jazzaro@temple.edu>
2019-05-21 20:36:26 -0700

    adding study outlines

--- javaScriptOutline.js
+++ javaScriptOutline.js
@@ -8,4 +8,70 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/* */
+/*
+
+
+•	
+    o	...
+
+
+
+CURRENT STUDIES
+///////////////
+
+STUDY OUTLINES
+///////////////
+
+
+•	API's, AJAX, and JSON with jQuery
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	Object Oriented Programming 
+    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/ObjOriProg.js
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+
+
+
+DATA STRUCTURES AND ALGORITHMS OUTLINES
+///////////////////////////////////////    
+•	How do you print the nth entry in a Fiboncacci series?
+    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/FibonacciSeries.js
+•	How do you write a function that accepts an integer and returns a NxN spiral matrix?
+    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/GenMatrixSpirals.js
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+•	
+    o	...
+*/
