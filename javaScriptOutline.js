49ca2e134e14fb192b422c43ff4fe34402dfdfcd
John Azzaro <jazzaro@temple.edu>
2019-05-21 20:48:53 -0700

    added outlines

--- javaScriptOutline.js
+++ javaScriptOutline.js
@@ -12,7 +12,7 @@
 
 
 •	
-    o	...
+        o	...
 
 
 
@@ -22,56 +22,65 @@ CURRENT STUDIES
 STUDY OUTLINES
 ///////////////
 
+•	Varables and Data Types
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/Variables%26DataTypes.js
 
 •	API's, AJAX, and JSON with jQuery
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/ApiAjaxJSon.js
+        o   https://github.com/john-azzaro/JavaScript_Recipes/blob/master/apiAjaxJson.html
+
 •	Object Oriented Programming 
-    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/ObjOriProg.js
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/ObjOriProg.js
+
+•	Basic Application Structure Guide
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/appStructure.js
+
+•	Application Logic
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/applicationLogic.js
+
+•	Arrays
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/arrays.js
+
+•	Control Flow
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/controlFlow.js
+
+•	Functions
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/functions.js
+
 •	
-    o	...
-•	
-    o	...
-•	
-    o	...
+        o	...
 
 
+https://github.com/john-azzaro/JavaScript_Recipes/blob/master/funStru.js
 
 DATA STRUCTURES AND ALGORITHMS OUTLINES
 ///////////////////////////////////////    
+
+
 •	How do you print the nth entry in a Fiboncacci series?
-    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/FibonacciSeries.js
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/FibonacciSeries.js
+
 •	How do you write a function that accepts an integer and returns a NxN spiral matrix?
-    o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/GenMatrixSpirals.js
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/GenMatrixSpirals.js
+
+•	How do you decide what solution is better using runtime complexity?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/RuntimeComplexity.js
+
+•	How do you check to see if two sentences have the same amount of characters?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/anagram.js
+
+•	How do you divide an array into chunks of a certain size?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/arrayChunk.js
+
+•	How do you write a function that returns the number of vowels ina string?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/findVowels.js
+
+•	How do you write a function that prints out word for multiples of X (fizzbuzz)?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/fizbuz.js
+
+•	How do you rveerse a string of numbers?
+        o	https://github.com/john-azzaro/JavaScript_Recipes/blob/master/integerReversal.js
+
 •	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
-•	
-    o	...
+        o	...
 */
