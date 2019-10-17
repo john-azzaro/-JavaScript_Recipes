dc9d889f0b3540bb3b4cf0524740b8efb80b8b64
John Azzaro <jazzaro@temple.edu>
2019-10-17 10:32:25 -0700

    updated concat

--- Array Methods/concat.js
+++ Array Methods/concat.js
@@ -21,16 +21,30 @@
 //      https://www.w3schools.com/jsref/jsref_concat_array.asp  --  w3schools outline of concat
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// EXAMPLE 1: Basic joining of two arrays using concat():
+    
+// SAMPLE DATA
 
     let myFirstArray = ["a","b","c","d","e","f","g","h","i","j",];
     let mySecondArray = [1,2,3,4,5,6,7,8,9,10];
+
+
+
+
+// EXAMPLE 1: Basic joining of two arrays using concat():
 
     function joinArrays(array1, array2) {
         return array1.concat(array2);
         
     }
-
     console.log(joinArrays(myFirstArray, mySecondArray));
+
+
+
+
+// EXAMPLE 2: Another basic joining of two arrays using concat():
+
+    function joinArrays2(array1, array2) {
+        const arrayCombo = array1.concat(array2);
+        return arrayCombo;
+    }
+    console.log(joinArrays2(myFirstArray, mySecondArray));
