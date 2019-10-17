590cb866ece0d22b9e1eef2dd8154a3271ddb522
John Azzaro <jazzaro@temple.edu>
2019-10-16 18:02:31 -0700

    updated format for concat

--- Array Methods/concat.js
+++ Array Methods/concat.js
@@ -1,9 +1,36 @@
-let myFirstArray = ["a","b","c","d","e","f","g","h","i","j",];
-let mySecondArray = [1,2,3,4,5,6,7,8,9,10];
+'use strict';
+// TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//     Array Method: .concat()
+//
+// SYNTAX ////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//     array1.concat(array2, array3, array4, ..., arrayX);      
+//
+// SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      • .concat() is used to join several arrays together.
+//      • .concat() does not change existing arrays but rather returns a new array containing the joined arrays.  
+//
+// EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      EXAMPLE 1: Basic joining of two arrays using concat():
+//
+// RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
+//     
+//      https://www.w3schools.com/jsref/jsref_concat_array.asp  --  w3schools outline of concat
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function joinArrays(array1, array2) {
-    return array1.concat(array2);
-    
-}
 
-console.log(joinArrays(myFirstArray, mySecondArray));
+// EXAMPLE 1: Basic joining of two arrays using concat():
+
+    let myFirstArray = ["a","b","c","d","e","f","g","h","i","j",];
+    let mySecondArray = [1,2,3,4,5,6,7,8,9,10];
+
+    function joinArrays(array1, array2) {
+        return array1.concat(array2);
+        
+    }
+
+    console.log(joinArrays(myFirstArray, mySecondArray));
