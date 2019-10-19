a2b0f8053e3aca87ce82f380d999b61f198b35e2
John Azzaro <jazzaro@temple.edu>
2019-10-18 22:43:07 -0700

    updated copyWithin

--- Array Methods/copyWithin.js
+++ Array Methods/copyWithin.js
@@ -9,8 +9,29 @@
 //
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      • .copyWithin copies an array of elements to another position in the array, overwriting existing values.
+//      •   .copyWithin copies an array of elements to another position in the array, overwriting existing values.
+//      •   The first argument is the TARGET, which is where the method will overwrite.
+//          •   If you have array.copyWithin(1, 0):
+//              BEFORE: ["alpha", "beta", "charlie", "delta", "echo", "frank"]
+//              AFTER:  ['alpha', 'alpha', 'beta', 'charlie', 'delta', 'echo' ]
+//                                  ^^^
+//          •   If you have array.copyWithin(2, 0):
+//              BEFORE: ["alpha", "beta", "charlie", "delta", "echo", "frank"]
+//              AFTER:  [ 'alpha', 'beta', 'alpha', 'beta', 'charlie', 'delta' ]
+//                                           ^^^
+//          •   If you have array.copyWithin(3, 0):
+//              BEFORE: ["alpha", "beta", "charlie", "delta", "echo", "frank"]//
+//              AFTER:  [ 'alpha', 'beta', 'charlie', 'alpha', 'beta', 'charlie' ]
+//                                                      ^^^
 //
+//
+//
+//
+//
+//
+//
+//
+
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      EXAMPLE 1: 
@@ -21,12 +42,12 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// EXAMPLE 1:
+// EXAMPLE 1: using copyWithin to overwrite from the the second array item (i.e. 1) position.
 
-const array = ["alpha", "beta", "charlie", "delta", "echo", "frank", ]
+const array = ["alpha", "beta", "charlie", "delta", "echo", "frank"];
 
 function copyWithinIt() {
-    return array.copyWithin(1, 0)
+    return array.copyWithin(3, 0)
 }
 
 console.log(copyWithinIt());
@@ -36,6 +57,8 @@ console.log(copyWithinIt());
 
 /* 
 So what's happening here? 
+
+
 
 
 */
