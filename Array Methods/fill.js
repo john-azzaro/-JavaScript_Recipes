b1694b94ee3d8eaae96393e762189cbc77ce6591
John Azzaro <jazzaro@temple.edu>
2019-10-22 09:54:00 -0700

    fill example

--- Array Methods/fill.js
+++ Array Methods/fill.js
@@ -11,13 +11,35 @@
 //
 //      • .fill is a function that is used to fill an array with a given static value which can be used ot fill
 //        the entire array or a part of the array.
+//      • In other words, with fill you can replace whole parts or sections of an array with a given value.
+//
+//      • .fill takes three arguments: value, start, end
+//          •   Value: The value that will replace elements in the array. 
+//          •   Start (optional): The starting point in the array that will fill the array.
+//          •   End (optional): The endingpoint in the array that will fill the array.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      EXAMPLE 1:
+//      EXAMPLE 1: Replace all the elements in the array with a given value.
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// EXAMPLE 1: Replace all the elements in the array with a given value.
+
+function replaceElements() {
+    let array = ["alpha", "beta", "charlie", "delta", "echo", ];
+    return array.fill("ALPHA");
+}
+
+console.log(replaceElements());
+
+// EXAMPLE 2: 
+
+
+
