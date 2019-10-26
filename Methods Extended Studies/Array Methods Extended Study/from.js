e17b76a99b51aa9fd2e09caeaaab409f66409056
John Azzaro <jazzaro@temple.edu>
2019-10-25 21:53:39 -0700

    updated string example

--- Methods Extended Studies/Array Methods Extended Study/from.js
+++ Methods Extended Studies/Array Methods Extended Study/from.js
@@ -1,17 +1,17 @@
 'use strict';
 // TOPIC /////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//     Array Method: .from
+//     Array Method: Array.from
 //
 // SYNTAX ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//     array.every(function(currentValue, index, arr), thisValue)
+//     let myArr = Array.from("ABCDEFG");
 //
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      • .from() is an inbuilt function which creates a new array from a given array.
-//      • .from() will convert every letter in the string into its own array instance.
-//      • .from() will take every integer value and make its own array.
+//      • Array.from() is an inbuilt function which creates a new array from a given array.
+//      • Array.from() will convert every letter in the string into its own array instance.
+//      • Array.from() will take every integer value and make its own array.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -20,5 +20,35 @@
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+//      https://www.w3schools.com/jsref/jsref_from.asp
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const myString = "This is a string";
+const myIntegers = [1,872,49,80,724];
+
+function testFrom(input) {
+    let myArray = Array.from(input);
+    return myArray;
+}
+
+console.log(testFrom(myString));           /* [ 'T',
+                                                'h',
+                                                'i',
+                                                's',
+                                                ' ',
+                                                'i',
+                                                's',
+                                                ' ',
+                                                'a',
+                                                ' ',
+                                                's',
+                                                't',
+                                                'r',
+                                                'i',
+                                                'n',
+                                                'g' ]    */
+
+console.log(testFrom(myIntegers));
+
