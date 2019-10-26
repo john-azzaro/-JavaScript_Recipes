8c2b84f2330988ac66d6ae8637dc350857594abf
John Azzaro <jazzaro@temple.edu>
2019-10-26 11:44:35 -0700

    updated from

--- Methods Extended Studies/Array Methods Extended Study/from.js
+++ Methods Extended Studies/Array Methods Extended Study/from.js
@@ -13,6 +13,9 @@
 //      • Array.from() is an inbuilt function which creates a new array from a given array.
 //      • Array.from() will convert every letter in the string into its own array instance.
 //      • Array.from() will take every integer value and make its own array.
+//      • Array.from() is a static method introduced in ES6 enabling a new shallow-copied array from
+//        an array type objector an iterable object.
+//      • With Array.from, you can create an "Array" instance.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -22,6 +25,7 @@
 //
 //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 //      https://www.w3schools.com/jsref/jsref_from.asp
+//      https://medium.com/javascript-everyday/javascript-array-from-53287c195487
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -30,7 +34,7 @@ const myString = "This is a string";
 
 
 function testFrom(input) {
-    let myArray = Array.from(input);
+    let myArray = Array.from(input).toString();
     return myArray;
 }
 
@@ -52,4 +56,6 @@ console.log(testFrom(myString));           /* [ 'T',
                                                 'g' ]    */
 
 
+
+                                                
 
