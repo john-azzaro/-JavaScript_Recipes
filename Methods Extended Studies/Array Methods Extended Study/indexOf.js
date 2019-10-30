66ae6696b5dbf5ff168b1f58a222fe22f86ce8cb
John Azzaro <jazzaro@temple.edu>
2019-10-29 18:55:19 -0700

    added examples with required and position

--- Methods Extended Studies/Array Methods Extended Study/indexOf.js
+++ Methods Extended Studies/Array Methods Extended Study/indexOf.js
@@ -23,3 +23,24 @@
 //      
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// EXAMPLE 1: Find the first instance of a given element in an array.
+
+const array = ['alan', 'bob', 'charlie', 'bob', 'elliot', 'alan', 'greg', 'hank', 'ian', 'jon', ];
+
+function findFirst(name, array) {
+    let find = array.indexOf(name);
+    return find;
+}
+
+console.log(findFirst('bob', array));       // position 1 (second element in the array since 0 is the first).block
+
+
+
+function findFirstAfterStart(name, array, startPosition) {
+    let find = array.indexOf(name, startPosition);
+    return find;
+}
+
+console.log(findFirstAfterStart('alan', array, 2));    // position 5, which is the second instance of alan in the array!
