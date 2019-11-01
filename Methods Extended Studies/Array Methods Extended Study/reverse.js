b7c259a83960c50103851419f0703092c73ae694
John Azzaro <jazzaro@temple.edu>
2019-11-01 08:00:34 -0700

    added reversed array example

--- Methods Extended Studies/Array Methods Extended Study/reverse.js
+++ Methods Extended Studies/Array Methods Extended Study/reverse.js
@@ -21,3 +21,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// EXAMPLE 1: Reverse an array:
+
+const myArray = [1,2,3,4,5,6,7,8,9,10];
+
+function reverseArray(array) {
+const reversedArray = array.reverse();
+console.log(reversedArray);
+}
+
+reverseArray(myArray);                // [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
