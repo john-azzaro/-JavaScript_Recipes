7b8f1915d07885ffb270c5584e296b10ec4dcfec
John Azzaro <jazzaro@temple.edu>
2019-10-31 17:03:58 -0700

    added string example

--- Methods Extended Studies/Array Methods Extended Study/values.js
+++ Methods Extended Studies/Array Methods Extended Study/values.js
@@ -10,10 +10,12 @@
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      • Object.values() is used to return enumerable property values of a simple array.
+//      • Object.values() will take an array and find the values in the array (i.e. apple, berry, carrot);
+//      • Object.values() will take a string in split it into seperate elements (i.e. puppy => p,u,p,p,y)
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      EXAMPLE 1:
+//      EXAMPLE 1: Find the values in an array.
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -22,18 +24,30 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// EXAMPLE 1: Find the values in an array.
+
 let array = ['alan', 'bob', 'charlie', 'dan', 'ellie', 'frank', 'greg', 'hank', 'ian'];
-let string = 
 
-
-function checkValues(arrayInput) {
-    let values = Object.values(arrayInput);
+function findValues(arrayInput) {
+    const values = Object.values(arrayInput);
     console.log(values);
 }
 
-console.log(checkValues());
+console.log(findValues(array));
 
 
 
-// var check = ['x', 'y', 'z'];
-//         console.log(Object.values(check));
+
+// EXAMPLE 2: Fund the values in a string.
+
+let theString = "puppy";
+
+function findValuesInString(stringInput) {
+    const values = Object.values(stringInput);
+    console.log(values);
+}
+
+console.log(findValuesInString(theString));
+
+
+
