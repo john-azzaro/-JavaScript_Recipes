708cabcb5ef4683b674dc3d1afe61ee2a2f309e3
John Azzaro <jazzaro@temple.edu>
2019-11-08 11:05:12 -0800

    added localeString examples

--- Methods Extended Studies/Array Methods Extended Study/toLocaleString.js
+++ Methods Extended Studies/Array Methods Extended Study/toLocaleString.js
@@ -13,6 +13,8 @@
 //        to a string.
 //      • .toLocaleString() needs the "dateObj" to be a valie Date object, such as:
 //              let event = new Date(Date.UTC(2019, 11, 20, 3, 0, 0));
+//      • For the OPTIONAL "locales" parameter, you use specific language or locale formats (i.e. 'en-US').  
+//      • For UTC, follow this format: Date.UTC(year[, month[, day[, hour[, minute[, second[, millisecond]]]]]])
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -24,3 +26,13 @@
 //     https://www.geeksforgeeks.org/javascript-date-tolocalestring/
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var date1 = new Date(Date());
+var date2 = new Date(Date());
+var date3 = new Date(Date.UTC(2014, 11, 25, 8, 20, 3));
+
+
+console.log(date1.toLocaleDateString());                    // 2019-11-8    -- returns current date
+console.log(date2.toLocaleDateString('en-US'));             // 11/8/2019    -- returns current date in US format
+console.log(date3.toLocaleDateString());                    // 11/8/2019    -- returns a date (but is off in the month for some reason)
+
