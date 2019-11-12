fcc5bf082c7b9b2d9d8ca0c120252cb6df40d4c4
John Azzaro <jazzaro@temple.edu>
2019-11-12 09:38:57 -0800

    added example

--- Methods Extended Studies/Array Methods Extended Study/pop.js
+++ Methods Extended Studies/Array Methods Extended Study/pop.js
@@ -5,18 +5,33 @@
 //
 // SYNTAX ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//     
+//     array.pop()
 //
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      • .pop() removes an element from the END of an array
+//      • .pop() removes the last item in an array.
+//      • .pop() returns the last item in the array.
+//      • .pop() changes the length of the array.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      EXAMPLE 1: 
+//      EXAMPLE 1: Remove the last element from an array.
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      
+//      https://alligator.io/js/push-pop-shift-unshift-array-methods/
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+const myArray = ['alan', 'bob', 'charlie', 'dan', 'eliot', 'frank', 'greg'];
+
+function removeLastItem(array) {
+    const removeItem = array.pop();
+    console.log(removeItem);
+}
+
+console.log(myArray);                 // [ 'alan', 'bob', 'charlie', 'dan', 'eliot', 'frank', 'greg' ]
+removeLastItem(myArray);              // greg
+console.log(myArray);                 // [ 'alan', 'bob', 'charlie', 'dan', 'eliot', 'frank' ]
