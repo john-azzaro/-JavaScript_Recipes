86866daf324f0828cbc15c196384a376d14b9c8e
John Azzaro <jazzaro@temple.edu>
2019-11-13 21:04:56 -0800

    updated

--- Methods Extended Studies/Array Methods Extended Study/spread.js
+++ Methods Extended Studies/Array Methods Extended Study/spread.js
@@ -6,49 +6,57 @@
 // SYNTAX ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      myFunction(...iterableObj);
-//      
+//
 //
 //
 //
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      • the spread operator is used to : expand arrays, combine arrays, and copy arrays and objects.
+//      • When you use the spread operator, you are essentially saying you want to take everything in either in
+//        an array or to be converted into an array and
 //      • When you use the spread operator to expand an array, you take the items in an array and "expand"
 //        out of the array. In other words, each item in the array is convereted to a string.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      EXAMPLE 1:
+//      EXAMPLE 1: Use the spread operator to convert an array into a string
+//      EXAMPLE 2: Use spread operator to convert a string into an array
+//
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//
+//      https://stackabuse.com/spread-operator-in-javascript/
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 // EXAMPLE 1: Use the spread operator to convert an array into a string
 
-const myArray1 = ["Hello", "there", "friend"];
+let myArray1 = ["Hello", "there", "friend"];
 
 function spreadIt1(array) {
-    console.log(...array);
+  console.log(...array);
 }
 
-console.log(myArray1);                            // [ 'Hello', 'there', 'friend' ]
-spreadIt1(myArray1);                              // Hello there friend
+console.log(myArray1); // [ 'Hello', 'there', 'friend' ]
+spreadIt1(myArray1); // Hello there friend
 
+// EXAMPLE 2: USe spread operator to convert a string into an array
 
-
-
-// EXAMPLE 2: 
-
-const myString = "Howdy there partner";
+let myString = "Howdy there partner";
 
 function spreadIt2(array) {
-    let convertToArray = [...array];
-    console.log(convertToArray);
+  let convertToArray = [...array];
+  console.log(convertToArray);
 }
 
-console.log(myString);                     // Howdy there partner
-spreadIt2(myString);                       //  [ 'H','o','w','d','y',' ','t','h','e','r','e',' ','p','a','r','t','n','e','r' ]
+console.log(myString); // Howdy there partner
+spreadIt2(myString); //  [ 'H','o','w','d','y',' ','t','h','e','r','e',' ','p','a','r','t','n','e','r' ]
+
+// EXAMPLE 3:
+
+const spread1 = [1, 2, 3, 4];
+const spread2 = [5, 6, 7, 8];
+
+const combinedSpread = [...spread1, ...spread2];
+console.log(combinedSpread); //=> [ 1, 2, 3, 4, 5, 6, 7, 8 ]
