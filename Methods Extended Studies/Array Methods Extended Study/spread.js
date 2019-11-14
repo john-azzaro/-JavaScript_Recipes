e6fe8e5a67d9fcfccf522138b981373beacb46a9
John Azzaro <jazzaro@temple.edu>
2019-11-13 21:15:11 -0800

    spread example updated

--- Methods Extended Studies/Array Methods Extended Study/spread.js
+++ Methods Extended Studies/Array Methods Extended Study/spread.js
@@ -22,7 +22,7 @@
 //
 //      EXAMPLE 1: Use the spread operator to convert an array into a string
 //      EXAMPLE 2: Use spread operator to convert a string into an array
-//
+//      EXAMPLE 3: Combine the elements of 2 arrays into a single array.
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -41,7 +41,7 @@ function spreadIt1(array) {
 console.log(myArray1); // [ 'Hello', 'there', 'friend' ]
 spreadIt1(myArray1); // Hello there friend
 
-// EXAMPLE 2: USe spread operator to convert a string into an array
+// EXAMPLE 2: Use spread operator to convert a string into an array
 
 let myString = "Howdy there partner";
 
@@ -53,10 +53,11 @@ function spreadIt2(array) {
 console.log(myString); // Howdy there partner
 spreadIt2(myString); //  [ 'H','o','w','d','y',' ','t','h','e','r','e',' ','p','a','r','t','n','e','r' ]
 
-// EXAMPLE 3:
+// EXAMPLE 3: Combine the elements of 2 arrays into a single array.
 
 const spread1 = [1, 2, 3, 4];
 const spread2 = [5, 6, 7, 8];
 
 const combinedSpread = [...spread1, ...spread2];
-console.log(combinedSpread); //=> [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+console.log(combinedSpread); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+console.log(...combinedSpread); // 1 2 3 4 5 6 7 8
