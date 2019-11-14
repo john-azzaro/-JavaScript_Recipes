8d95a125814b91a8e7376be8c48a0f3f159e5768
John Azzaro <jazzaro@temple.edu>
2019-11-13 20:42:57 -0800

    update spread example 1

--- Methods Extended Studies/Array Methods Extended Study/spread.js
+++ Methods Extended Studies/Array Methods Extended Study/spread.js
@@ -14,7 +14,7 @@
 //
 //      • the spread operator is used to : expand arrays, combine arrays, and copy arrays and objects.
 //      • When you use the spread operator to expand an array, you take the items in an array and "expand"
-//         
+//        out of the array. In other words, each item in the array is convereted to a string.
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -25,3 +25,20 @@
 //
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// EXAMPLE 1: Use the spread operator to convert an array into a string
+
+const myArray1 = ["Hello", "there", "friend"];
+
+function spreadIt1(array) {
+    console.log(...array);
+}
+
+console.log(myArray1);                            // [ 'Hello', 'there', 'friend' ]
+spreadIt1(myArray1);                              // Hello there friend
+
+
+
+
+// EXAMPLE 2: 
