ed18fc50dc261e7372c2e0cbea35175fbd58de67
John Azzaro <jazzaro@temple.edu>
2019-11-15 14:00:03 -0800

    updated map with practical example

--- Methods Extended Studies/Array Methods Extended Study/map.js
+++ Methods Extended Studies/Array Methods Extended Study/map.js
@@ -6,18 +6,20 @@
 // SYNTAX ////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      const myVariable = array.map(function(value) {
-//          return value + 1                 
+//          return value + 1;                 
 //      });                                                
 //
 // SUMMARY ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //      • .map() is used to generate a new a new array of items by applying the same function to each item 
 //        in the array. In other words, we can map each item in the array to something else.
-//      • 
+//      • .map() 
 //
 // EXAMPLES //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//      EXAMPLE 1:
+//      EXAMPLE 1: Map an array of numbers using a function containing an argument
+//      
+//
 //
 // RESOURCES /////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -27,7 +29,7 @@
 
 
 
-// EXAMPLE 1: Apply
+// EXAMPLE 1: Map an array of numbers using a function containing an argument:
 
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -40,3 +42,24 @@ function doMap(array) {
 
 console.log(myArray);                             // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 doMap(myArray);                                   // [ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ]
+
+
+
+// EXAMPLE 2: Create an unordered list of items from an array:
+
+const myGroceries = ['apples', 'bread', 'carrots', 'dough'];
+
+function convertArrayToList(array) {
+    const createListItem = array.map(function(item) {
+        return '<li>' + item + '</li>'
+    });
+    console.log(createListItem);                                     //[ '<li>apples</li>','<li>bread</li>','<li>carrots</li>','<li>dough</li>' ]
+    
+    const convertArrayToString = createListItem.join(' ');
+    console.log(convertArrayToString);                               // <li>apples</li> <li>bread</li> <li>carrots</li> <li>dough</li>
+
+    const wrapInUlTag = '<ul> ' + convertArrayToString + ' </ul>'   
+    console.log(wrapInUlTag);                                        // <ul> <li>apples</li> <li>bread</li> <li>carrots</li> <li>dough</li> </ul>
+}
+
+convertArrayToList(myGroceries);
