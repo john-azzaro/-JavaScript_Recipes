ea053930d392a9b38f3ee348676b0b4cbae4b4d2
John Azzaro <jazzaro@temple.edu>
2019-11-19 20:46:06 -0800

    updated endsWith

--- Methods Extended Studies/Array Methods Extended Study/endsWith.js
+++ Methods Extended Studies/Array Methods Extended Study/endsWith.js
@@ -23,8 +23,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// EXAMPLE 1: 
-
+// EXAMPLE 1: Create a function that evaluates the end value of a string as either true or false. 
 
 const myString = "This is a string and it is amazing";
 
@@ -32,3 +31,5 @@ function endsWithValue(string, searchValue) {
   const endsWith = string.endsWith(searchValue);
   console.log(endsWith);
 }
+
+endsWithValue(myString, 'amazing');              // true
