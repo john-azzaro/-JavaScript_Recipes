38a71d97a5df52474005ffb36a08bcf91601d3cf
John Azzaro <jazzaro@temple.edu>
2020-03-06 20:30:21 -0800

    updated

--- JavaScript Problem Solving/veryEasy.js
+++ JavaScript Problem Solving/veryEasy.js
@@ -14,7 +14,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* 
-Title:Is the Last Character an N?
+Is the Last Character an N?
 Create a function that takes a string (a random name). If the last character of the name is an "n", return true, otherwise return false.
 /////////////////////////////////////////////
 */
