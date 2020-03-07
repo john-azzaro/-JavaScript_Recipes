ec6b5fa0d1ba4a793fa240fe41bb5d48e933a0dc
John Azzaro <jazzaro@temple.edu>
2020-03-06 22:53:10 -0800

    updated

--- JavaScript Problem Solving/veryEasy.js
+++ JavaScript Problem Solving/veryEasy.js
@@ -26,3 +26,78 @@ function isLastCharacterN(word) {
 		return false;
 	}
 }
+
+
+/*
+Check String for Spaces
+Create a function that returns true if a string contains any spaces.
+////////////////////////////////////////////////////////////////////
+*/
+function hasSpaces(str) {
+	if (str.includes(" ")) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/*
+Multiply Every Array Item by Two
+Create a function that takes an array with numbers and return an array with the elements multiplied by two.
+////////////////////////////////////////////////////////////////////
+*/
+function getMultipliedArr(arr) {
+	let multi = arr.map(function(num) {
+		return num * 2;
+	});
+	return multi;
+}
+
+
+/*
+Extract City Facts
+Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
+name, population, continent
+The string should have the following format: X has a population of Y and is situated in Z (where X is the city name, Y is the population and Z is the continent the city is situated in).
+////////////////////////////////////////////////////////////////////
+*/
+function cityFacts(city) {
+	let getCity = city.name;
+	let getPopulation = city.population;
+	let getContinent = city.continent;
+	return `${getCity} has a population of ${getPopulation} and is situated in ${getContinent}`;
+}
+
+
+/*
+Add a Consecutive List of Numbers
+Write a function that takes the last number of a consecutive list of numbers and returns the total of all numbers up to and including it.
+////////////////////////////////////////////////////////////////////
+*/
+function addUpTo(n) {
+	return n * (n + 1) / 2;
+}
+
+
+/*
+Shapes With N Sides
+Create a function that takes a whole number as input and returns the shape with that number's amount of sides. Here are the expected outputs to get from these inputs.
+Note: I initially did a large if/else array but this seems a lot better.
+////////////////////////////////////////////////////////////////////
+*/
+function nSidedShape(n) {
+	let shapes = {
+		1:	"circle",
+		2:	"semi-circle",
+		3:	"triangle",
+		4:	"square",
+		5:	"pentagon",
+		6:	"hexagon",
+		7:	"heptagon",
+		8:	"octagon",
+		9:	"nonagon",
+		10:	"decagon",
+	}
+	return shapes[n];
+}
